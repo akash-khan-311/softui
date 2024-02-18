18c10b8966a0717a1a9702db961b191f48038b56
Akash Khan <akashcc48@gmail.com>
2024-02-18 14:30:09 +0600

    navbar setuped

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,73 +1,98 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
+import { Collapse, Typography, IconButton } from "@material-tailwind/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 const Navbar = () => {
-  const NavList = (
-    <ul className="flex flex-col gap-2 my-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6 ">
-      <li className="block p-1 font-sans text-sm antialiased font-medium leading-normal ">
-        <Link
-         
-          className="flex items-center transition-colors hover:text-blue-500"
+  const [openNav, setOpenNav] = useState(false);
+  const handleWindowResize = () =>
+    window.innerWidth >= 960 && setOpenNav(false);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+  const NavList = () => {
+    return (
+      <ul className="my-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+        <Typography
+          as="li"
+          variant="large"
+          color="white"
+          className="p-1 font-medium"
         >
-          Components
-        </Link>
-      </li>
-      <li className="block p-1 font-sans text-sm antialiased font-medium leading-normal ">
-        <Link
-         
-          className="flex items-center transition-colors hover:text-blue-500"
+          <Link
+            to={"/components"}
+            className="flex items-center hover:text-blue-500 transition-colors"
+          >
+            Components
+          </Link>
+        </Typography>
+        <Typography
+          as="li"
+          variant="large"
+          color="white"
+          className="p-1 font-medium"
         >
-          Blocks
-        </Link>
-      </li>
-      <li className="block p-1 font-sans text-sm antialiased font-medium leading-normal ">
-        <Link
-         
-          className="flex items-center transition-colors hover:text-blue-500"
+          <Link
+            to={"/blocks"}
+            className="flex items-center hover:text-blue-500 transition-colors"
+          >
+            Account
+          </Link>
+        </Typography>
+        <Typography
+          as="li"
+          variant="large"
+          color="white"
+          className="p-1 font-medium"
         >
-          Documantion
-        </Link>
-      </li>
-     
-    </ul>
-  );
+          <Link
+            to={"/Documantion"}
+            className="flex items-center hover:text-blue-500 transition-colors"
+          >
+            Documantion
+          </Link>
+        </Typography>
+      </ul>
+    );
+  };
+
   return (
-    <nav className="block w-full  px-6 py-3 mx-auto text-white border shadow-md  border-white/20 bg-opacity-80 backdrop-blur-2xl backdrop-saturate-200">
-      <div className="flex items-center justify-between ">
-        <Link to={'/'}
-         
-          className="mr-4 block cursor-pointer py-1.5 font-sans text-4xl font-semibold leading-relaxed tracking-normal text-inherit antialiased"
-        >
-          Aero UI
+    <nav className="mx-auto bg-blue-gray-900 ">
+      <div className="flex items-center justify-between container mx-auto">
+        <Link to={'/'}>
+          <Typography
+            as="a"
+            color="white"
+            variant="h3"
+            className="mr-4 cursor-pointer py-1.5"
+          >
+            Aero UI
+          </Typography>
         </Link>
         <div className="hidden lg:block">
-            {NavList}
+          <NavList />
         </div>
-        <div>
-        <button>Get Started</button>
-        </div>
-        <button
-          className="relative ml-auto h-6 max-h-[40px] w-6 max-w-[40px] select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase text-inherit transition-all hover:bg-transparent focus:bg-transparent active:bg-transparent disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:hidden"
-          type="button"
+        <div></div>
+        <IconButton
+          variant="text"
+          className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+          ripple={false}
+          onClick={() => setOpenNav(!openNav)}
         >
-          <span className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={2}
-              stroke="currentColor"
-              aria-hidden="true"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-              />
-            </svg>
-          </span>
-        </button>
+          {openNav ? (
+            <XMarkIcon className="h-6 w-6" strokeWidth={2} />
+          ) : (
+            <Bars3Icon className="h-6 w-6" strokeWidth={2} />
+          )}
+        </IconButton>
       </div>
+      <Collapse open={openNav}>
+        <NavList />
+      </Collapse>
     </nav>
   );
 };
