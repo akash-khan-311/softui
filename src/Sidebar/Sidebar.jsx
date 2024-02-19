9aaf5dd40cdcb06ed1d945c5ab103a21d00b7cea
Akash Khan <akashcc48@gmail.com>
2024-02-19 11:11:57 +0600

    add more items nav

--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable no-unused-vars */
 import { FaAngleDown } from "react-icons/fa";
 import { MdMenuBook } from "react-icons/md";
+import { RiSettings4Fill } from "react-icons/ri";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+const componentsItems = ['Avatar', 'Accordion', 'Button', 'Carousel', 'Card', 'Form']
 const Sidebar = () => {
   const [docOpen, seDocOpen] = useState(true);
   const [componentOpen, setComponentOpen] = useState(true);
@@ -12,8 +14,8 @@ const Sidebar = () => {
   
       className={`sidebar fixed z-40  w-[300px] px-6 select-none block transition-all overflow-y-scroll`}
     >
-      <div className="group flex h-full w-full flex-col space-y-6  ">
-        {/* components container */}
+      <div className="group flex h-full w-full flex-col space-y-2 ">
+        {/* docs container */}
         <div className="flex flex-col px-4 pt-10 text-lg text-white ">
           <div onClick={() => seDocOpen(!docOpen)} className="text-2xl">
             <div className="flex justify-between items-center border px-4 py-2 rounded-md overflow-hidden">
@@ -47,6 +49,42 @@ const Sidebar = () => {
                 >
                   Quick Start
                 </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* components container */}
+        <div className="flex flex-col px-4 text-lg text-white ">
+          <div onClick={() => setComponentOpen(!componentOpen)} className="text-2xl">
+            <div className="flex justify-between items-center border px-4 py-2 rounded-md overflow-hidden">
+              <div className="flex items-center ">
+                <RiSettings4Fill className="mr-3 text-3xl" /> <span>Docs</span>
+              </div>
+              <FaAngleDown
+                className={`transition-all duration-300 ease-in-out text-yellow-900  ${
+                    componentOpen ? "rotate-180" : "rotate-0"
+                }`}
+              />
+            </div>
+
+            <div
+              className={`transition-all duration-300 ease-in-out grid ml-3  ${
+                componentOpen
+                  ? "grid-rows-[1fr] opacity-100"
+                  : " grid grid-rows-[0fr] opacity-0"
+              } `}
+            >
+              <div className="my-2 flex flex-col space-y-1 overflow-hidden border-l border-yellow-900 text-lg px-4">
+                {
+                    componentsItems.map((ItemName,i)=> <Link key={i}
+                        className="px-4 py-2 text-white duration-300 rounded-md hover:bg-yellow-900 hover:text-white"
+                        to={ItemName.toLocaleLowerCase()}
+                      >
+                         {ItemName}
+                      </Link>)
+                }
+                
+              
               </div>
             </div>
           </div>
