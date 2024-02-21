5d9548331bc543e7c39c915023ebe7525e489abb
Akash Khan <akashcc48@gmail.com>
2024-02-21 17:11:32 +0600

    another navbar added

--- src/UIcomponents/Navbar/Navbar.jsx
+++ src/UIcomponents/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
 import Navbar1 from "../../components/App/Navbar/Navbar1";
+import Navbar2 from "../../components/App/Navbar/Navbar2";
 
 const Navbar = () => {
   return (
     <div className="flex flex-col justify-center items-start">
-        
+      {/* Basic Navbar */}
       <Navbar1 />
+      {/* Between Nvbar */}
+      <Navbar2 />
     </div>
   );
 };
