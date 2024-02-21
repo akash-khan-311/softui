525089ca6b6995c4aeda4364595e2188f4623a2c
Akash Khan <akashcc48@gmail.com>
2024-02-21 15:18:20 +0600

    login form with social button added

--- src/UIcomponents/Form/Form.jsx
+++ src/UIcomponents/Form/Form.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import Form1 from "../../components/App/Forms/Form1";
+import Form2 from "../../components/App/Forms/Form2";
 
 const Form = () => {
   return (
@@ -7,7 +8,10 @@ const Form = () => {
       <h2 className="text-5xl text-center text-white mt-20">
         Let's Explore All Form
       </h2>
+      {/* Default Login Form */}
       <Form1 />
+      {/*Login Form With Social Button  */}
+      <Form2 />
     </div>
   );
 };
