2debf85f47533dca0deb8f220bae52113d4d0af8
Akash Khan <akashcc48@gmail.com>
2024-02-21 13:41:39 +0600

    modal components structured

--- src/UIcomponents/Modal/Modal.jsx
+++ src/UIcomponents/Modal/Modal.jsx
@@ -1,87 +1,11 @@
 /* eslint-disable react/no-unescaped-entities */
-import { useState } from "react";
-import CodeBox from "../../Shared/CodeBox/CodeBox";
+import Modal1 from "../../components/App/Modals/Modal1";
 
 const Modal = () => {
-  const [modalOpen, setModalOpen] = useState(false);
-  const modalcode = `import { useState } from "react";
-const Modal = () => {
-    const [modalOpen, setModalOpen] = useState(false);
-    return (
-        <div>
-            <button onClick={() => setModalOpen(true)} className="bg-gradient-to-tr from-blue-900 to-blue-500 rounded-md py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">Open Dialog</button>
-            <div onClick={() => setModalOpen(false)}  className={\`z-[999] fixed flex items-center justify-center h-screen w-screen place-items-center \${modalOpen ? "visible opacity-1" : "invisible opacity-0"} inset-0 bg-black bg-opacity-60 duration-100 \`}>
-                <div onClick={(e) => e.stopPropagation()} className={\`absolute m-4 w-2/5 min-w-[40%] max-w-[40%] rounded-lg bg-white font-sans text-base font-lightd text-blue-gray-500 \${modalOpen ? "translate-y-6 opacity-1 duration-300" : "-translate-y-6 opacity-0 duration-200"} shadow-2xl\`}>
-                    <div className="flex items-center p-4 font-sans text-2xl antialiased font-semibold leading-snug shrink-0 text-blue-gray-900">
-                        Hi, I'm Aero UI
-                    </div>
-                    <div className="relative p-4 font-sans text-base antialiased font-light leading-relaxed border-t border-b border-t-blue-gray-100 border-b-blue-gray-100 text-blue-gray-500">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat eveniet aperiam veritatis voluptate architecto vitae, exercitationem nostrum perspiciatis autem iure, odit alias odio. Suscipit eum sequi quod quidem est incidunt?
-                    </div>
-                    <div className="flex flex-wrap items-center justify-end p-4 shrink-0 text-blue-gray-500">
-                      <button onClick={() => setModalOpen(false)} className="px-6 py-3 mr-1 font-sans text-xs font-bold text-red-500 uppercase transition-all rounded-lg middle none center hover:bg-red-500/10 active:bg-red-500/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">Cancel</button>
-                      <button onClick={() => setModalOpen(false)} className="middle none center rounded-lg bg-gradient-to-tr from-green-600 to-green-400 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-green-500/20 transition-all hover:shadow-lg hover:shadow-green-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">Confirm</button>
-                    </div>
-                </div>
-            </div>
-          </div>
-        );
-    };
-export default Modal;
-    
-
-`;
   return (
-    <div className="">
-      <CodeBox stringCode={modalcode}>
-        <div>
-          <button
-            onClick={() => setModalOpen(true)}
-            className="bg-gradient-to-tr from-blue-900 to-blue-500 rounded-md py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-          >
-            Open Dialog
-          </button>
-          <div
-            onClick={() => setModalOpen(false)}
-            className={`z-[999] fixed flex items-center justify-center h-screen w-screen place-items-center ${
-              modalOpen ? "visible opacity-1" : "invisible opacity-0"
-            } inset-0 bg-black bg-opacity-60 duration-100`}
-          >
-            <div
-              onClick={(e) => e.stopPropagation()}
-              className={`absolute m-4 w-2/5 min-w-[40%] max-w-[40%] rounded-lg bg-white font-sans text-base font-lightd text-blue-gray-500 ${
-                modalOpen
-                  ? "translate-y-6 opacity-1 duration-300"
-                  : "-translate-y-6 opacity-0 duration-200"
-              } shadow-2xl`}
-            >
-              <div className="flex items-center p-4 font-sans text-2xl antialiased font-semibold leading-snug shrink-0 text-blue-gray-900">
-                Hi, I'm Aero UI
-              </div>
-              <div className="relative p-4 font-sans text-base antialiased font-light leading-relaxed border-t border-b border-t-blue-gray-100 border-b-blue-gray-100 text-blue-gray-500">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Repellat eveniet aperiam veritatis voluptate architecto vitae,
-                exercitationem nostrum perspiciatis autem iure, odit alias odio.
-                Suscipit eum sequi quod quidem est incidunt?
-              </div>
-              <div className="flex flex-wrap items-center justify-end p-4 shrink-0 text-blue-gray-500">
-                <button
-                  onClick={() => setModalOpen(false)}
-                  className="px-6 py-3 mr-1 font-sans text-xs font-bold text-red-500 uppercase transition-all rounded-lg middle none center hover:bg-red-500/10 active:bg-red-500/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                >
-                  Cancel
-                </button>
-                <button
-                  onClick={() => setModalOpen(false)}
-                  className="middle none center rounded-lg bg-gradient-to-tr from-green-600 to-green-400 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-green-500/20 transition-all hover:shadow-lg hover:shadow-green-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                >
-                  Confirm
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </CodeBox>
+    <div>
+      {/* Default Modal */}
+      <Modal1 />
     </div>
   );
 };
