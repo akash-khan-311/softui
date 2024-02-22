20a58424eb4a9f11442c3501df0cc623b62cdf44
Akash Khan <akashcc48@gmail.com>
2024-02-22 15:38:59 +0600

    some footer components added

--- src/UIcomponents/Footer/Footer.jsx
+++ src/UIcomponents/Footer/Footer.jsx
@@ -1,10 +1,19 @@
 import Footer1 from "../../components/App/Footers/Footer1";
+import Footer2 from "../../components/App/Footers/Footer2";
+import Footer3 from "../../components/App/Footers/Footer3";
+import Footer4 from "../../components/App/Footers/Footer4";
 
 const Footer = () => {
   return (
     <div>
-      {/* Basic Footer */}
+      {/* Copyright Footer */}
       <Footer1 />
+      {/* Footer Links */}
+      <Footer2 />
+      {/* Footer With Icons */}
+      <Footer3 />
+      {/*  */}
+      <Footer4 />
     </div>
   );
 };
---
+++ src/components/App/Footers/Footer2.jsx
@@ -0,0 +1,313 @@
+import CodeBox from "../../../Shared/CodeBox/CodeBox";
+
+const Footer2 = () => {
+  return (
+    <>
+      <CodeBox
+        text={"Footer With Links"}
+        stringCode={`<footer className="flex flex-col items-center bg-gray-100 text-center lg:text-left">
+  <div className="container p-6 border-b border-gray-300">
+    <div className="grid place-items-center md:grid-cols-2 lg:grid-cols-4 ">
+      {/* First links section */}
+      <div className="mb-6">
+        <h5 className="mb-2.5 font-bold uppercase text-neutral-800 dark:text-neutral-200">
+          Links
+        </h5>
+        <ul className="mb-0 list-none">
+          <li>
+            <a href="#!" className="text-neutral-800 dark:text-neutral-200">
+              Link 1
+            </a>
+          </li>
+          <li>
+            <a href="#!" className="text-neutral-800 dark:text-neutral-200">
+              Link 2
+            </a>
+          </li>
+          <li>
+            <a href="#!" className="text-neutral-800 dark:text-neutral-200">
+              Link 3
+            </a>
+          </li>
+          <li>
+            <a href="#!" className="text-neutral-800 dark:text-neutral-200">
+              Link 4
+            </a>
+          </li>
+        </ul>
+      </div>
+      {/* Second links section */}
+      <div className="mb-6">
+        <h5 className="mb-2.5 font-bold uppercase text-neutral-800 dark:text-neutral-200">
+          Links
+        </h5>
+        <ul className="mb-0 list-none">
+          <li>
+            <a href="#!" className="text-neutral-800 dark:text-neutral-200">
+              Link 1
+            </a>
+          </li>
+          <li>
+            <a href="#!" className="text-neutral-800 dark:text-neutral-200">
+              Link 2
+            </a>
+          </li>
+          <li>
+            <a href="#!" className="text-neutral-800 dark:text-neutral-200">
+              Link 3
+            </a>
+          </li>
+          <li>
+            <a href="#!" className="text-neutral-800 dark:text-neutral-200">
+              Link 4
+            </a>
+          </li>
+        </ul>
+      </div>
+      {/* Third links section */}
+      <div className="mb-6">
+        <h5 className="mb-2.5 font-bold uppercase text-neutral-800 dark:text-neutral-200">
+          Links
+        </h5>
+        <ul className="mb-0 list-none">
+          <li>
+            <a href="#!" className="text-neutral-800 dark:text-neutral-200">
+              Link 1
+            </a>
+          </li>
+          <li>
+            <a href="#!" className="text-neutral-800 dark:text-neutral-200">
+              Link 2
+            </a>
+          </li>
+          <li>
+            <a href="#!" className="text-neutral-800 dark:text-neutral-200">
+              Link 3
+            </a>
+          </li>
+          <li>
+            <a href="#!" className="text-neutral-800 dark:text-neutral-200">
+              Link 4
+            </a>
+          </li>
+        </ul>
+      </div>
+      {/* Fourth links section */}
+      <div className="mb-6">
+        <h5 className="mb-2.5 font-bold uppercase text-neutral-800 dark:text-neutral-200">
+          Links
+        </h5>
+        <ul className="mb-0 list-none">
+          <li>
+            <a href="#!" className="text-neutral-800 dark:text-neutral-200">
+              Link 1
+            </a>
+          </li>
+          <li>
+            <a href="#!" className="text-neutral-800 dark:text-neutral-200">
+              Link 2
+            </a>
+          </li>
+          <li>
+            <a href="#!" className="text-neutral-800 dark:text-neutral-200">
+              Link 3
+            </a>
+          </li>
+          <li>
+            <a href="#!" className="text-neutral-800 dark:text-neutral-200">
+              Link 4
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  {/* Copyright section */}
+  <div className="p-4 text-center text-neutral-700 dark:text-neutral-200 flex justify-center ">
+    <span className="mr-2">© 2023 Copyright</span>
+    <span className="text-gray-900 font-semibold cursor-pointer">
+      Aero UI
+    </span>
+  </div>
+</footer>
+`}
+      >
+        <footer className="flex flex-col items-center bg-gray-100 text-center  lg:text-left">
+          <div className="container p-6 border-b border-gray-300">
+            <div className="grid place-items-center md:grid-cols-2 lg:grid-cols-4 ">
+              {/*First links section*/}
+              <div className="mb-6">
+                <h5 className="mb-2.5 font-bold uppercase text-neutral-800 dark:text-neutral-200">
+                  Links
+                </h5>
+                <ul className="mb-0 list-none">
+                  <li>
+                    <a
+                      href="#!"
+                      className="text-neutral-800 dark:text-neutral-200"
+                    >
+                      Link 1
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#!"
+                      className="text-neutral-800 dark:text-neutral-200"
+                    >
+                      Link 2
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#!"
+                      className="text-neutral-800 dark:text-neutral-200"
+                    >
+                      Link 3
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#!"
+                      className="text-neutral-800 dark:text-neutral-200"
+                    >
+                      Link 4
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              {/*Second links section*/}
+              <div className="mb-6">
+                <h5 className="mb-2.5 font-bold uppercase text-neutral-800 dark:text-neutral-200">
+                  Links
+                </h5>
+                <ul className="mb-0 list-none">
+                  <li>
+                    <a
+                      href="#!"
+                      className="text-neutral-800 dark:text-neutral-200"
+                    >
+                      Link 1
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#!"
+                      className="text-neutral-800 dark:text-neutral-200"
+                    >
+                      Link 2
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#!"
+                      className="text-neutral-800 dark:text-neutral-200"
+                    >
+                      Link 3
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#!"
+                      className="text-neutral-800 dark:text-neutral-200"
+                    >
+                      Link 4
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              {/*Third links section*/}
+              <div className="mb-6">
+                <h5 className="mb-2.5 font-bold uppercase text-neutral-800 dark:text-neutral-200">
+                  Links
+                </h5>
+                <ul className="mb-0 list-none">
+                  <li>
+                    <a
+                      href="#!"
+                      className="text-neutral-800 dark:text-neutral-200"
+                    >
+                      Link 1
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#!"
+                      className="text-neutral-800 dark:text-neutral-200"
+                    >
+                      Link 2
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#!"
+                      className="text-neutral-800 dark:text-neutral-200"
+                    >
+                      Link 3
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#!"
+                      className="text-neutral-800 dark:text-neutral-200"
+                    >
+                      Link 4
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              {/*Fourth links section*/}
+              <div className="mb-6">
+                <h5 className="mb-2.5 font-bold uppercase text-neutral-800 dark:text-neutral-200">
+                  Links
+                </h5>
+                <ul className="mb-0 list-none">
+                  <li>
+                    <a
+                      href="#!"
+                      className="text-neutral-800 dark:text-neutral-200"
+                    >
+                      Link 1
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#!"
+                      className="text-neutral-800 dark:text-neutral-200"
+                    >
+                      Link 2
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#!"
+                      className="text-neutral-800 dark:text-neutral-200"
+                    >
+                      Link 3
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#!"
+                      className="text-neutral-800 dark:text-neutral-200"
+                    >
+                      Link 4
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          {/*Copyright section*/}
+          <div className="p-4 text-center text-neutral-700 dark:text-neutral-200 flex justify-center ">
+            <span className="mr-2">© 2023 Copyright</span>
+            <span className="text-gray-900 font-semibold cursor-pointer">
+              Aero UI
+            </span>
+          </div>
+        </footer>
+      </CodeBox>
+    </>
+  );
+};
+
+export default Footer2;
