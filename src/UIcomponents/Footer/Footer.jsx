acf801e832b29bc0e30f78033e53b82621225635
Akash Khan <akashcc48@gmail.com>
2024-02-22 15:13:20 +0600

    footer components added

--- src/UIcomponents/Footer/Footer.jsx
+++ src/UIcomponents/Footer/Footer.jsx
@@ -1,5 +1,12 @@
+import Footer1 from "../../components/App/Footers/Footer1";
+
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div>
+      {/* Basic Footer */}
+      <Footer1 />
+    </div>
+  );
 };
 
 export default Footer;
