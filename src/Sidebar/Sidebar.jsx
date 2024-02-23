868efaa4074e044977eb8eeb4c989de10e3683d2
Akash Khan <akashcc48@gmail.com>
2024-02-23 12:03:36 +0600

    sidebar design changes

--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -37,7 +37,7 @@ const Sidebar = () => {
   };
   return (
     <div
-      className={`components-sidebar fixed top-16  z-[80] -translate-x-full md:translate-x-0  w-[300px] select-none px-6 block transition-all  overflow-y-scroll 
+      className={`components-sidebar backdrop-blur-xl bg-white/10 fixed top-16 z-[80] -translate-x-full md:translate-x-0  w-[250px] select-none px-2 block transition-all  overflow-y-scroll 
       `}
     >
       <div className=" flex h-full w-full flex-col space-y-2  ">
@@ -46,14 +46,17 @@ const Sidebar = () => {
           <div onClick={handleDocNavOpen} className="text-2xl">
             <div className="flex justify-between items-center border px-4 py-2 rounded-md ">
               <div className="flex items-center ">
-                <MdMenuBook className="mr-3 text-xl" />{" "}
-                <span className="text-lg">Docs</span>
+                <FaAngleDown
+                  className={`transition-all text-sm mr-3 duration-300 ease-in-out text-yellow-900  ${
+                    docOpen ? "rotate-180" : "rotate-0"
+                  }`}
+                />
+
+                <span className="text-sm leading-2 tracking-wide">
+                  Getting Started
+                </span>
               </div>
-              <FaAngleDown
-                className={`transition-all duration-300 ease-in-out text-yellow-900  ${
-                  docOpen ? "rotate-180" : "rotate-0"
-                }`}
-              />
+              <MdMenuBook className="text-sm" />{" "}
             </div>
 
             <div
@@ -63,15 +66,15 @@ const Sidebar = () => {
                   : " grid grid-rows-[0fr] opacity-0"
               } `}
             >
-              <div className="my-2 flex flex-col space-y-1 overflow-hidden border-l border-yellow-900 text-lg px-4">
+              <div className="my-2 flex flex-col  overflow-hidden  text-lg space-y-3">
                 <Link
-                  className="px-4 py-2 text-white duration-300 rounded-md hover:bg-yellow-900 hover:text-white"
+                  className="px-2 text-gray-400 duration-300 text-sm leading-2 tracking-wide   hover:text-white border-l-2 border-transparent hover:border-l-2 hover:border-l-orange-500"
                   to={"/"}
                 >
                   Introduction
                 </Link>
                 <Link
-                  className="px-4 py-2 text-white duration-300 rounded-md hover:bg-yellow-900 hover:text-white"
+                  className="px-2  text-gray-400 duration-300  text-sm leading-2 tracking-wide  hover:text-white border-l-2 border-transparent hover:border-l-2 hover:border-l-orange-500"
                   to="/"
                 >
                   Quick Start
@@ -81,18 +84,21 @@ const Sidebar = () => {
           </div>
         </div>
         {/* components container */}
-        <div className="flex flex-col px-4 text-lg text-white ">
+        <div className="flex flex-col px-4 text-lg ">
           <div onClick={handleComponentNavOpen} className="text-2xl">
             <div className="flex justify-between items-center border px-4 py-2 rounded-md overflow-hidden">
               <div className="flex items-center ">
-                <RiSettings4Fill className="mr-3 text-xl" />{" "}
-                <span className="text-lg">Components</span>
+                <FaAngleDown
+                  className={`transition-all text-sm duration-300 mr-3 ease-in-out text-yellow-900  ${
+                    componentOpen ? "rotate-180" : "rotate-0"
+                  }`}
+                />
+
+                <span className="text-sm leading-2 tracking-wide text-white">
+                  Components
+                </span>
               </div>
-              <FaAngleDown
-                className={`transition-all duration-300 ease-in-out text-yellow-900  ${
-                  componentOpen ? "rotate-180" : "rotate-0"
-                }`}
-              />
+              <RiSettings4Fill className=" text-sm text-white" />{" "}
             </div>
 
             <div
@@ -102,12 +108,12 @@ const Sidebar = () => {
                   : " grid grid-rows-[0fr] opacity-0"
               } `}
             >
-              <div className="my-2 flex flex-col space-y-1 overflow-hidden border-l border-yellow-900 text-lg px-4">
+              <div className="my-2 flex flex-col overflow-hidden text-sm space-y-3 ">
                 {componentsItems.map((ItemName, i) => (
                   <NavLink
                     onClick={handleLink}
                     key={i}
-                    className="px-4 py-2 text-white duration-300 rounded-md hover:bg-yellow-900 hover:text-white"
+                    className="px-2 text-gray-400 duration-300  hover:text-white border-l-2 border-transparent hover:border-l-2 hover:border-l-orange-500"
                     to={ItemName.toLocaleLowerCase()}
                   >
                     {ItemName}
