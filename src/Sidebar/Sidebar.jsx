ce47af6ccaeb33f1039825623d0f5aac9976bd41
Akash Khan <akashcc48@gmail.com>
2024-02-23 21:19:04 +0600

    sidebar styles

--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -37,10 +37,10 @@ const Sidebar = () => {
   };
   return (
     <div
-      className={`components-sidebar backdrop-blur-xl bg-white/10 fixed top-16 z-[80] -translate-x-full md:translate-x-0  w-[250px] select-none px-2 block transition-all  overflow-y-scroll 
+      className={`components-sidebar backdrop-blur-xl bg-white/10 fixed top-16 z-[80] -translate-x-full md:translate-x-0  w-[250px] select-none px-2 block transition-all overflow-y-scroll 
       `}
     >
-      <div className=" flex h-full w-full flex-col space-y-2  ">
+      <div className="flex h-full w-full flex-col space-y-2  ">
         {/* docs container */}
         <div className="flex flex-col px-4 pt-10 text-lg text-white ">
           <div onClick={handleDocNavOpen} className="text-2xl">
