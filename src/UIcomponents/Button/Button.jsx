be41ce4cea280c123bdbc72d38c3f711d99ab085
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-02-23 17:00:26 +0600

    Button 5 added

--- src/UIcomponents/Button/Button.jsx
+++ src/UIcomponents/Button/Button.jsx
@@ -2,15 +2,16 @@ import Button1 from "../../components/App/Buttons/Button1";
 import Button2 from "../../components/App/Buttons/Button2";
 import Button3 from "../../components/App/Buttons/Button3";
 import Button4 from "../../components/App/Buttons/Button4";
+import Button5 from "../../components/App/Buttons/Button5";
 
 const Button = () => {
   return (
     <div className="flex flex-col gap-5 justify-center items-start">
-      {/* Button 1 */}
       <Button1 />
       <Button2 />
       <Button3 />
       <Button4 />
+      <Button5 />
     </div>
   );
 };
--- src/components/App/Avatars/Avatars1.jsx
+++ src/components/App/Avatars/Avatars1.jsx
@@ -13,17 +13,17 @@ const Avatars1 = () => {
         <div className="flex flex-wrap gap-5 justify-evenly items-center ">
           <img
             className="w-16 h-16 rounded-full"
-            src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg"
+            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
             alt="Rounded avatar"
           />
           <img
             className="w-16 h-16 rounded"
-            src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg"
+            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
             alt="Default avatar"
           />
           <img
             className="w-16 h-16 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500"
-            src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg"
+            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
             alt="Bordered avatar"
           />
         </div>
