215f7a1fc708f4acd16a628f0a47d5ab369d7c55
nayon117 <hasibul.nayon1@gmail.com>
2024-02-24 12:40:43 +0600

    create statistics-4

--- src/UIcomponents/Stats/Stats.jsx
+++ src/UIcomponents/Stats/Stats.jsx
@@ -1,6 +1,7 @@
 import Stats1 from "../../components/App/Stats/Stats1";
 import Stats2 from "../../components/App/Stats/Stats2";
 import Stats3 from "../../components/App/Stats/Stats3";
+import Stats4 from "../../components/App/Stats/Stats4";
 
 const Stats = () => {
     return(
@@ -8,6 +9,7 @@ const Stats = () => {
              <Stats1/>
              <Stats2/>
              <Stats3/>
+             <Stats4/>
         </div>
     )
 }
