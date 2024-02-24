55507419eac0b1935310806f656f781aab7af605
Akash Khan <akashcc48@gmail.com>
2024-02-24 14:34:50 +0600

    some spinner components added

--- src/components/App/Spinner/Spinner1.jsx
+++ src/components/App/Spinner/Spinner1.jsx
@@ -1,9 +1,24 @@
+import CodeBox from "../../../Shared/CodeBox/CodeBox";
 
 const Spinner1 = () => {
-    return(
-        <div>
-             <p> This is Spinner1 </p>
+  return (
+    <div>
+      <CodeBox
+        stringCode={`<div className="flex flex-row gap-2">
+  <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.7s]" />
+  <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.3s]" />
+  <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.7s]" />
+</div>`}
+      >
+        <div className="flex justify-center py-12">
+          <div className="flex flex-row gap-2">
+            <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.3s]" />
+            <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.1s]" />
+            <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.3s]" />
+          </div>
         </div>
-    )
-}
+      </CodeBox>
+    </div>
+  );
+};
 export default Spinner1;
