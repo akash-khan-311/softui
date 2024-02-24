9529ef6189bec1d9b435427a3d31016fee7b84f7
Akash Khan <akashcc48@gmail.com>
2024-02-24 15:20:52 +0600

    some buttons components added

--- src/UIcomponents/Button/Button.jsx
+++ src/UIcomponents/Button/Button.jsx
@@ -3,15 +3,17 @@ import Button2 from "../../components/App/Buttons/Button2";
 import Button3 from "../../components/App/Buttons/Button3";
 import Button4 from "../../components/App/Buttons/Button4";
 import Button5 from "../../components/App/Buttons/Button5";
+import Button6 from "../../components/App/Buttons/Button6";
 
 const Button = () => {
   return (
-    <div className="flex flex-col gap-5 justify-center items-start">
+    <div className="flex flex-col gap-5 ">
       <Button1 />
       <Button2 />
       <Button3 />
       <Button4 />
       <Button5 />
+      <Button6 />
     </div>
   );
 };
---
+++ src/components/App/Buttons/Button7.jsx
@@ -0,0 +1,9 @@
+
+const Button7 = () => {
+    return(
+        <div>
+             <p> This is Button7 </p>
+        </div>
+    )
+}
+export default Button7;
