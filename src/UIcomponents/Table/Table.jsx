15d659c7434a236b784fb10df1673f2f1875ef6a
Akash Khan <akashcc48@gmail.com>
2024-02-25 14:01:32 +0600

    table components added

---
+++ src/UIcomponents/Table/Table.jsx
@@ -0,0 +1,12 @@
+import Table1 from "../../components/App/Tables/Table1";
+import Table2 from "../../components/App/Tables/Table2";
+
+const Table = () => {
+    return(
+        <div>
+             <Table1/>
+             <Table2/>
+        </div>
+    )
+}
+export default Table;
