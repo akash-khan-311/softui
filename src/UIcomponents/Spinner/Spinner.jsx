582e4d815c991d47ae4a1ab26ccb6ec25bddf8df
nayon117 <hasibul.nayon1@gmail.com>
2024-02-25 12:36:46 +0600

    add spinner4

--- src/UIcomponents/Spinner/Spinner.jsx
+++ src/UIcomponents/Spinner/Spinner.jsx
@@ -1,6 +1,7 @@
 import Spinner1 from "../../components/App/Spinner/Spinner1";
 import Spinner2 from "../../components/App/Spinner/Spinner2";
 import Spinner3 from "../../components/App/Spinner/Spinner3";
+import Spinner4 from "../../components/App/Spinner/Spinner4";
 
 const Spinner = () => {
   return (
@@ -8,6 +9,7 @@ const Spinner = () => {
       <Spinner1 />
       <Spinner2/>
       <Spinner3/>
+      <Spinner4/>
     </div>
   );
 };
