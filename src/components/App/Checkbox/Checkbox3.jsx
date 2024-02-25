48bd1e93a33788afc147965807e004207493eff4
Akash Khan <akashcc48@gmail.com>
2024-02-25 13:05:22 +0600

    checkbox aligment changes

--- src/components/App/Checkbox/Checkbox3.jsx
+++ src/components/App/Checkbox/Checkbox3.jsx
@@ -2,14 +2,15 @@ import { useState } from "react";
 import CodeBox from "../../../Shared/CodeBox/CodeBox";
 
 const Checkbox3 = () => {
-  const [isChecked, setIsChecked] = useState(false)
+  const [isChecked, setIsChecked] = useState(false);
 
   const handleCheckboxChange = () => {
-    setIsChecked(!isChecked)
-  }
+    setIsChecked(!isChecked);
+  };
   return (
     <>
-      <CodeBox stringCode={`
+      <CodeBox
+        stringCode={`
      import { useState } from 'react'
 
      export const Checkbox1 = () => {
@@ -37,30 +38,31 @@ const Checkbox3 = () => {
        )
      }
       
-      `}>
-      <label className='flex items-center cursor-pointer select-none text-white '>
-      <div className='relative'>
-        <input
-          type='checkbox'
-          checked={isChecked}
-          onChange={handleCheckboxChange}
-          className='sr-only'
-        />
-        <div className='box mr-4 flex h-5 w-5 items-center justify-center rounded-full border border-stroke'>
-        <span
-            className={`h-[10px] w-[10px] rounded-full ${
-              isChecked ? 'bg-blue-500' : 'bg-transparent'
-            }`}
-          >
-            {' '}
-          </span>
+      `}
+      >
+        <div className="flex justify-center items-center">
+          <label className="flex items-center cursor-pointer select-none text-white ">
+            <div className="relative">
+              <input
+                type="checkbox"
+                checked={isChecked}
+                onChange={handleCheckboxChange}
+                className="sr-only"
+              />
+              <div className="box mr-4 flex h-5 w-5 items-center justify-center rounded-full border border-stroke">
+                <span
+                  className={`h-[10px] w-[10px] rounded-full ${
+                    isChecked ? "bg-blue-500" : "bg-transparent"
+                  }`}
+                >
+                  {" "}
+                </span>
+              </div>
+            </div>
+            CheckboxText
+          </label>
         </div>
-      </div>
-      CheckboxText
-    </label>
-
       </CodeBox>
-     
     </>
   );
 };
