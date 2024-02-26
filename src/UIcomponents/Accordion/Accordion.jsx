1946e0fc3f3c79d021307ad1ee56bcc29e7cb543
Akash Khan <akashcc48@gmail.com>
2024-02-26 18:55:42 +0600

    another accordion added

--- src/UIcomponents/Accordion/Accordion.jsx
+++ src/UIcomponents/Accordion/Accordion.jsx
@@ -1,10 +1,12 @@
 import Accordions1 from "../../components/App/Accordions/Accordions1";
+import Accordion2 from "../../components/App/Accordions/Accordion2";
 
 const Accordion = () => {
   return (
     <>
       {/* Accordion 1 */}
       <Accordions1 />
+      <Accordion2/>
     </>
   );
 };
