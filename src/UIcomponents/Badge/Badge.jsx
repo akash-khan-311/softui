907270c159fc69967cb4de8955d505a3edd0c942
Akash Khan <akashcc48@gmail.com>
2024-02-27 21:33:32 +0600

    multiple badge added

--- src/UIcomponents/Badge/Badge.jsx
+++ src/UIcomponents/Badge/Badge.jsx
@@ -1,8 +1,10 @@
 import Badge1 from "../../components/App/Badges/Badge1"
+import Badge2 from "../../components/App/Badges/Badge2";
 const Badge = () => {
     return(
         <div>
              <Badge1/>
+             <Badge2/>
         </div>
     )
 }
