dcb86a2b54df7d19cf903627f241631bac4c28f2
Akash Khan <akashcc48@gmail.com>
2024-02-28 13:38:42 +0600

    another alert added

--- src/UIcomponents/Alert/Alert.jsx
+++ src/UIcomponents/Alert/Alert.jsx
@@ -1,11 +1,15 @@
 import Alert1 from "../../components/App/Alerts/Alert1";
 import Alert2 from "../../components/App/Alerts/Alert2";
+import Alert3 from "../../components/App/Alerts/Alert3";
+import Alert4 from "../../components/App/Alerts/Alert4";
 
 const Alert = () => {
     return(
         <div>
              <Alert1/>
              <Alert2/>
+             <Alert4/>
+             <Alert3/>
         </div>
     )
 }
