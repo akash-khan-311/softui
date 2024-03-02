acc50be65e7b40a7431d9cc79ddfb05a60d87a01
Akash Khan <akashcc48@gmail.com>
2024-03-02 12:02:11 +0600

    some progress bar added

--- src/UIcomponents/Progress/Progress.jsx
+++ src/UIcomponents/Progress/Progress.jsx
@@ -1,9 +1,15 @@
 import Progress1 from "../../components/App/Progress/Progress1";
+import Progress2 from "../../components/App/Progress/Progress2";
+import Progress3 from "../../components/App/Progress/Progress3";
+import Progress4 from "../../components/App/Progress/Progress4";
 
 const Progress = () => {
     return(
         <>
              <Progress1/>
+             <Progress2/>
+             <Progress3/>
+             <Progress4/>
         </>
     )
 }
---
+++ src/components/App/Progress/Progress4.jsx
@@ -0,0 +1,120 @@
+import CodeBox from "../../../Shared/CodeBox/CodeBox";
+
+const Progress4 = () => {
+    return(
+        <>
+             <CodeBox text={'Color Progress'} stringCode={`<div className="mb-7">
+    <div className="flex justify-between py-1">
+      <span className="text-base font-semibold text-white">Web Design</span>
+      <span className="text-base font-semibold pr-5 text-white">80%</span>
+    </div>
+    <svg className="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
+      <path className="rc-progress-line-trail" d="M 0.5,0.5
+     L 99.5,0.5" strokeLinecap="round" stroke="#D9D9D9" strokeWidth={1} fillOpacity={0} />
+      <path className="rc-progress-line-path" d="M 0.5,0.5
+     L 99.5,0.5" strokeLinecap="round" stroke="#FF6464" strokeWidth={1} fillOpacity={0} style={{strokeDasharray: '79.2px, 100px', strokeDashoffset: 0, transition: 'stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s'}}>
+      </path>
+    </svg>
+</div>
+
+<div className="mb-7">
+  <div className="flex justify-between py-1">
+    <span className="text-base font-semibold text-white">Mobile App </span>
+    <span className="text-base font-semibold pr-5 text-white">95%</span>
+  </div>
+  <svg className="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
+    <path className="rc-progress-line-trail" d="M 0.5,0.5
+   L 99.5,0.5" strokeLinecap="round" stroke="#D9D9D9" strokeWidth={1} fillOpacity={0} />
+    <path className="rc-progress-line-path" d="M 0.5,0.5
+   L 99.5,0.5" strokeLinecap="round" stroke="#9272D4" strokeWidth={1} fillOpacity={0} style={{strokeDasharray: '94.05px, 100px', strokeDashoffset: 0, transition: 'stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s'}}>
+    </path>
+  </svg>
+</div>
+
+<div className="mb-7">
+  <div className="flex justify-between py-1">
+    <span className="text-base font-semibold text-white">Illustrator</span>
+    <span className="text-base font-semibold pr-5 text-white">65%</span>
+  </div>
+  <svg className="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
+    <path className="rc-progress-line-trail" d="M 0.5,0.5
+   L 99.5,0.5" strokeLinecap="round" stroke="#D9D9D9" strokeWidth={1} fillOpacity={0} />
+    <path className="rc-progress-line-path" d="M 0.5,0.5
+   L 99.5,0.5" strokeLinecap="round" stroke="#5185D4" strokeWidth={1} fillOpacity={0} style={{strokeDasharray: '64.35px, 100px', strokeDashoffset: 0, transition: 'stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s'}}>
+    </path>
+  </svg>
+</div>
+
+<div className="mb-7">
+  <div className="flex justify-between py-1">
+    <span className="text-base font-semibold text-white">Photoshope</span>
+    <span className="text-base font-semibold pr-5 text-white">75%</span>
+  </div>
+  <svg className="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
+    <path className="rc-progress-line-trail" d="M 0.5,0.5
+   L 99.5,0.5" strokeLinecap="round" stroke="#D9D9D9" strokeWidth={1} fillOpacity={0} />
+    <path className="rc-progress-line-path" d="M 0.5,0.5
+   L 99.5,0.5" strokeLinecap="round" stroke="#CA56F2" strokeWidth={1} fillOpacity={0} style={{strokeDasharray: '74.25px, 100px', strokeDashoffset: 0, transition: 'stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s'}}>
+    </path>
+  </svg>
+</div>`}>
+             <div className="max-w-xl mx-auto p-4">
+  <div className="mb-7">
+    <div className="flex justify-between py-1">
+      <span className="text-base font-semibold text-white">Web Design</span>
+      <span className="text-base font-semibold pr-5 text-white">80%</span>
+    </div>
+    <svg className="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
+      <path className="rc-progress-line-trail" d="M 0.5,0.5
+     L 99.5,0.5" strokeLinecap="round" stroke="#D9D9D9" strokeWidth={1} fillOpacity={0} />
+      <path className="rc-progress-line-path" d="M 0.5,0.5
+     L 99.5,0.5" strokeLinecap="round" stroke="#FF6464" strokeWidth={1} fillOpacity={0} style={{strokeDasharray: '79.2px, 100px', strokeDashoffset: 0, transition: 'stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s'}}>
+      </path>
+    </svg>
+  </div>
+  <div className="mb-7">
+    <div className="flex justify-between py-1">
+      <span className="text-base font-semibold text-white">Mobile App </span>
+      <span className="text-base font-semibold pr-5 text-white">95%</span>
+    </div>
+    <svg className="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
+      <path className="rc-progress-line-trail" d="M 0.5,0.5
+     L 99.5,0.5" strokeLinecap="round" stroke="#D9D9D9" strokeWidth={1} fillOpacity={0} />
+      <path className="rc-progress-line-path" d="M 0.5,0.5
+     L 99.5,0.5" strokeLinecap="round" stroke="#9272D4" strokeWidth={1} fillOpacity={0} style={{strokeDasharray: '94.05px, 100px', strokeDashoffset: 0, transition: 'stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s'}}>
+      </path>
+    </svg>
+  </div>
+  <div className="mb-7">
+    <div className="flex justify-between py-1">
+      <span className="text-base font-semibold text-white">Illustrator</span>
+      <span className="text-base font-semibold pr-5 text-white">65%</span>
+    </div>
+    <svg className="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
+      <path className="rc-progress-line-trail" d="M 0.5,0.5
+     L 99.5,0.5" strokeLinecap="round" stroke="#D9D9D9" strokeWidth={1} fillOpacity={0} />
+      <path className="rc-progress-line-path" d="M 0.5,0.5
+     L 99.5,0.5" strokeLinecap="round" stroke="#5185D4" strokeWidth={1} fillOpacity={0} style={{strokeDasharray: '64.35px, 100px', strokeDashoffset: 0, transition: 'stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s'}}>
+      </path>
+    </svg>
+  </div>
+  <div className="mb-7">
+    <div className="flex justify-between py-1">
+      <span className="text-base font-semibold text-white">Photoshope</span>
+      <span className="text-base font-semibold pr-5 text-white">75%</span>
+    </div>
+    <svg className="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
+      <path className="rc-progress-line-trail" d="M 0.5,0.5
+     L 99.5,0.5" strokeLinecap="round" stroke="#D9D9D9" strokeWidth={1} fillOpacity={0} />
+      <path className="rc-progress-line-path" d="M 0.5,0.5
+     L 99.5,0.5" strokeLinecap="round" stroke="#CA56F2" strokeWidth={1} fillOpacity={0} style={{strokeDasharray: '74.25px, 100px', strokeDashoffset: 0, transition: 'stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s'}}>
+      </path>
+    </svg>
+  </div>
+</div>
+
+             </CodeBox>
+        </>
+    )
+}
+export default Progress4;
