f60ad6d67178a52190fb93edfdbded676d5a1a90
Akash Khan <akashcc48@gmail.com>
2024-03-02 15:33:20 +0600

    cta style changes

--- src/UIcomponents/Button/Button.jsx
+++ src/UIcomponents/Button/Button.jsx
@@ -3,6 +3,7 @@ import Button10 from "../../components/App/Buttons/Button10";
 import Button11 from "../../components/App/Buttons/Button11";
 import Button12 from "../../components/App/Buttons/Button12";
 import Button13 from "../../components/App/Buttons/Button13";
+import Button14 from "../../components/App/Buttons/Button14";
 import Button2 from "../../components/App/Buttons/Button2";
 import Button3 from "../../components/App/Buttons/Button3";
 import Button4 from "../../components/App/Buttons/Button4";
@@ -12,23 +13,23 @@ import Button7 from "../../components/App/Buttons/Button7";
 import Button8 from "../../components/App/Buttons/Button8";
 import Button9 from "../../components/App/Buttons/Button9";
 
-
 const Button = () => {
   return (
     <div className="flex flex-col gap-5 ">
       <Button1 />
       <Button2 />
-      <Button13/>
+      <Button13 />
       <Button3 />
       <Button4 />
       <Button5 />
       <Button6 />
       <Button7 />
       <Button8 />
-      <Button9/>
-      <Button10/>
-      <Button11/>
-      <Button12/>
+      <Button9 />
+      <Button10 />
+      <Button11 />
+      <Button12 />
+      <Button14 />
     </div>
   );
 };
--- src/UIcomponents/Drawer/Drawer.jsx
+++ src/UIcomponents/Drawer/Drawer.jsx
@@ -1,16 +1,18 @@
+
 import Drawer1 from "../../components/App/Drawers/Drawer1";
 import Drawer2 from "../../components/App/Drawers/Drawer2";
 import Drawer3 from "../../components/App/Drawers/Drawer3";
 import Drawer4 from "../../components/App/Drawers/Drawer4";
 
 const Drawer = () => {
-    return(
-        <>
-             <Drawer1/>
-             <Drawer2/>
-             <Drawer3/>
-             <Drawer4/>
-        </>
-    )
-}
+  return (
+    <>
+      <Drawer1 />
+      <Drawer2 />
+      <Drawer3 />
+      <Drawer4 />
+      
+    </>
+  );
+};
 export default Drawer;
