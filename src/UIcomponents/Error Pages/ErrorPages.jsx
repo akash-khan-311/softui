d1a6f05c0b0f36a8e41242885370ee8b0f56673e
Akash Khan <akashcc48@gmail.com>
2024-03-05 13:33:12 +0600

    error page 4 added

--- src/UIcomponents/Error Pages/ErrorPages.jsx
+++ src/UIcomponents/Error Pages/ErrorPages.jsx
@@ -1,6 +1,7 @@
 import ErrorPage1 from "../../components/App/Error Pages/ErrorPage1";
 import ErrorPage2 from "../../components/App/Error Pages/ErrorPage2";
 import ErrorPage3 from "../../components/App/Error Pages/ErrorPage3";
+import ErrorPage4 from "../../components/App/Error Pages/ErrorPage4";
 
 
 const ErrorPages = () => {
@@ -9,6 +10,7 @@ const ErrorPages = () => {
             <ErrorPage1/>
             <ErrorPage2/>
             <ErrorPage3/>
+            <ErrorPage4/>
         </>
     )
 }
