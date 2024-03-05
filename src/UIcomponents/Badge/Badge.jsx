f4f509ff7406ba3ad4696e314d664c5264cd35be
Najmul Huda Chowdhury Chayan <najmulhc@outlook.com>
2024-03-05 11:29:38 +0600

    Added rounded badges in the list.

--- src/UIcomponents/Badge/Badge.jsx
+++ src/UIcomponents/Badge/Badge.jsx
@@ -2,6 +2,7 @@ import Badge1 from "../../components/App/Badges/Badge1"
 import Badge2 from "../../components/App/Badges/Badge2";
 import Badge3 from "../../components/App/Badges/Badge3";
 import Badge4 from "../../components/App/Badges/Badge4";
+import Badge5 from "../../components/App/Badges/Badge5";
 const Badge = () => {
     return(
         <div>
@@ -9,6 +10,7 @@ const Badge = () => {
              <Badge2/>
              <Badge3/>
              <Badge4/>
+             <Badge5/>
         </div>
     )
 }
