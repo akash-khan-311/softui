525fae17b92634f2c31bb7cab621ac5b0fe5627c
Akash Khan <akashcc48@gmail.com>
2024-03-06 12:22:06 +0600

    some code fixed

--- src/Shared/ScrollToTop/ScrollToTop.jsx
+++ src/Shared/ScrollToTop/ScrollToTop.jsx
@@ -22,7 +22,7 @@ const ScrollToTop = () => {
   return (
     <>
       <button
-        className={` transition-all ease-in-out duration-300 animate-bounce ${isVissible ? "opacity-100 " : "opacity-0 "} z-20 text-white mt-32 fixed bottom-10 right-2 p-1 rounded-full bg-gradient-to-tr from-purple-900 to-pink-900 `}
+        className={` transition-all ease-in-out duration-300 animate-bounce ${isVissible ? "opacity-100 " : "opacity-0 "} z-20 text-white mt-32 fixed bottom-10 right-2 md:right-5 p-1 rounded-full bg-gradient-to-tr from-purple-900 to-pink-900 `}
         onClick={scrolToTopHandler}
       >
         <span>
