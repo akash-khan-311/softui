a99b9e1d20a2118274be68ca96ebc339fafc31cb
Akash Khan <akashcc48@gmail.com>
2024-03-07 16:51:31 +0600

    check box and toggle button added

---
+++ src/components/App/Toggles/Toggle3.jsx
@@ -0,0 +1,203 @@
+import CodeBox from "../../../Shared/CodeBox/CodeBox";
+
+const Toggle3 = () => {
+    return(
+        <>
+             <CodeBox stringCode={`<div className="mb-3">
+  <div className="relative inline-block w-10 mr-2 align-middle select-none">
+    <input type="checkbox" name="toggle" id="Blue" className="checked:bg-blue-500 outline-none focus:outline-none right-4 checked:right-0 duration-200 ease-in absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer" />
+    <label htmlFor="Blue" className="block h-6 overflow-hidden bg-gray-500 rounded-full cursor-pointer">
+    </label>
+  </div>
+  <span className="font-medium text-gray-400">
+    Blue
+  </span>
+</div>
+
+<div className="mb-3">
+  <div className="relative inline-block w-10 mr-2 align-middle select-none">
+    <input type="checkbox" name="toggle" id="Pink" className="checked:bg-pink-500 outline-none focus:outline-none right-4 checked:right-0 duration-200 ease-in absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer" />
+    <label htmlFor="Pink" className="block h-6 overflow-hidden bg-gray-500 rounded-full cursor-pointer">
+    </label>
+  </div>
+  <span className="font-medium text-gray-400">
+    Pink
+  </span>
+</div>
+
+<div className="mb-3">
+  <div className="relative inline-block w-10 mr-2 align-middle select-none">
+    <input type="checkbox" name="toggle" id="Green" className="checked:bg-green-500 outline-none focus:outline-none right-4 checked:right-0 duration-200 ease-in absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer" />
+    <label htmlFor="Green" className="block h-6 overflow-hidden bg-gray-500 rounded-full cursor-pointer">
+    </label>
+  </div>
+  <span className="font-medium text-gray-400">
+    Green
+  </span>
+</div>
+
+<div className="mb-3">
+  <div className="relative inline-block w-10 mr-2 align-middle select-none">
+    <input type="checkbox" name="toggle" id="Orange" className="checked:bg-yellow-500 outline-none focus:outline-none right-4 checked:right-0 duration-200 ease-in absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer" />
+    <label htmlFor="Orange" className="block h-6 overflow-hidden bg-gray-500 rounded-full cursor-pointer">
+    </label>
+  </div>
+  <span className="font-medium text-gray-400">
+    Orange
+  </span>
+</div>
+
+<div className="mb-3">
+  <div className="relative inline-block w-10 mr-2 align-middle select-none">
+    <input type="checkbox" name="toggle" id="Purple" className="checked:bg-purple-500 outline-none focus:outline-none right-4 checked:right-0 duration-200 ease-in absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer" />
+    <label htmlFor="Purple" className="block h-6 overflow-hidden bg-gray-500 rounded-full cursor-pointer">
+    </label>
+  </div>
+  <span className="font-medium text-gray-400">
+    Purple
+  </span>
+</div>
+
+<div className="mb-3">
+  <div className="relative inline-block w-10 mr-2 align-middle select-none">
+    <input type="checkbox" name="toggle" id="Black" className="checked:bg-black outline-none focus:outline-none right-4 checked:right-0 duration-200 ease-in absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer" />
+    <label htmlFor="Black" className="block h-6 overflow-hidden bg-gray-500 rounded-full cursor-pointer">
+    </label>
+  </div>
+  <span className="font-medium text-gray-400">
+    Black
+  </span>
+</div>
+
+<div className="mb-3">
+  <div className="relative inline-block w-10 mr-2 align-middle select-none">
+    <input type="checkbox" name="toggle" id="Indigo" className="checked:bg-indigo-500 outline-none focus:outline-none right-4 checked:right-0 duration-200 ease-in absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer" />
+    <label htmlFor="Indigo" className="block h-6 overflow-hidden bg-gray-500 rounded-full cursor-pointer">
+    </label>
+  </div>
+  <span className="font-medium text-gray-400">
+    Indigo
+  </span>
+</div>
+
+<div className="mb-3">
+  <div className="relative inline-block w-10 mr-2 align-middle select-none">
+    <input type="checkbox" name="toggle" id="Red" className="checked:bg-red-500 outline-none focus:outline-none right-4 checked:right-0 duration-200 ease-in absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer" />
+    <label htmlFor="Red" className="block h-6 overflow-hidden bg-gray-500 rounded-full cursor-pointer">
+    </label>
+  </div>
+  <span className="font-medium text-gray-400">
+    Red
+  </span>
+</div>
+
+<div className="mb-3">
+  <div className="relative inline-block w-10 mr-2 align-middle select-none">
+    <input type="checkbox" name="toggle" id="Gray" className="checked:bg-gray-500 outline-none focus:outline-none right-4 checked:right-0 duration-200 ease-in absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer" />
+    <label htmlFor="Gray" className="block h-6 overflow-hidden bg-gray-500 rounded-full cursor-pointer">
+    </label>
+  </div>
+  <span className="font-medium text-gray-400">
+    Gray
+  </span>
+</div>`}>
+                
+<div className="flex  justify-center gap-10 flex-wrap">
+  <div className="mb-3">
+    <div className="relative inline-block w-10 mr-2 align-middle select-none">
+      <input type="checkbox" name="toggle" id="Blue" className="checked:bg-blue-500 outline-none focus:outline-none right-4 checked:right-0 duration-200 ease-in absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer" />
+      <label htmlFor="Blue" className="block h-6 overflow-hidden bg-gray-500 rounded-full cursor-pointer">
+      </label>
+    </div>
+    <span className="font-medium text-gray-400">
+      Blue
+    </span>
+  </div>
+  <div className="mb-3">
+    <div className="relative inline-block w-10 mr-2 align-middle select-none">
+      <input type="checkbox" name="toggle" id="Pink" className="checked:bg-pink-500 outline-none focus:outline-none right-4 checked:right-0 duration-200 ease-in absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer" />
+      <label htmlFor="Pink" className="block h-6 overflow-hidden bg-gray-500 rounded-full cursor-pointer">
+      </label>
+    </div>
+    <span className="font-medium text-gray-400">
+      Pink
+    </span>
+  </div>
+  <div className="mb-3">
+    <div className="relative inline-block w-10 mr-2 align-middle select-none">
+      <input type="checkbox" name="toggle" id="Green" className="checked:bg-green-500 outline-none focus:outline-none right-4 checked:right-0 duration-200 ease-in absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer" />
+      <label htmlFor="Green" className="block h-6 overflow-hidden bg-gray-500 rounded-full cursor-pointer">
+      </label>
+    </div>
+    <span className="font-medium text-gray-400">
+      Green
+    </span>
+  </div>
+  <div className="mb-3">
+    <div className="relative inline-block w-10 mr-2 align-middle select-none">
+      <input type="checkbox" name="toggle" id="Orange" className="checked:bg-yellow-500 outline-none focus:outline-none right-4 checked:right-0 duration-200 ease-in absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer" />
+      <label htmlFor="Orange" className="block h-6 overflow-hidden bg-gray-500 rounded-full cursor-pointer">
+      </label>
+    </div>
+    <span className="font-medium text-gray-400">
+      Orange
+    </span>
+  </div>
+  <div className="mb-3">
+    <div className="relative inline-block w-10 mr-2 align-middle select-none">
+      <input type="checkbox" name="toggle" id="Purple" className="checked:bg-purple-500 outline-none focus:outline-none right-4 checked:right-0 duration-200 ease-in absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer" />
+      <label htmlFor="Purple" className="block h-6 overflow-hidden bg-gray-500 rounded-full cursor-pointer">
+      </label>
+    </div>
+    <span className="font-medium text-gray-400">
+      Purple
+    </span>
+  </div>
+  <div className="mb-3">
+    <div className="relative inline-block w-10 mr-2 align-middle select-none">
+      <input type="checkbox" name="toggle" id="Black" className="checked:bg-black outline-none focus:outline-none right-4 checked:right-0 duration-200 ease-in absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer" />
+      <label htmlFor="Black" className="block h-6 overflow-hidden bg-gray-500 rounded-full cursor-pointer">
+      </label>
+    </div>
+    <span className="font-medium text-gray-400">
+      Black
+    </span>
+  </div>
+  <div className="mb-3">
+    <div className="relative inline-block w-10 mr-2 align-middle select-none">
+      <input type="checkbox" name="toggle" id="Indigo" className="checked:bg-indigo-500 outline-none focus:outline-none right-4 checked:right-0 duration-200 ease-in absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer" />
+      <label htmlFor="Indigo" className="block h-6 overflow-hidden bg-gray-500 rounded-full cursor-pointer">
+      </label>
+    </div>
+    <span className="font-medium text-gray-400">
+      Indigo
+    </span>
+  </div>
+  <div className="mb-3">
+    <div className="relative inline-block w-10 mr-2 align-middle select-none">
+      <input type="checkbox" name="toggle" id="Red" className="checked:bg-red-500 outline-none focus:outline-none right-4 checked:right-0 duration-200 ease-in absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer" />
+      <label htmlFor="Red" className="block h-6 overflow-hidden bg-gray-500 rounded-full cursor-pointer">
+      </label>
+    </div>
+    <span className="font-medium text-gray-400">
+      Red
+    </span>
+  </div>
+  <div className="mb-3">
+    <div className="relative inline-block w-10 mr-2 align-middle select-none">
+      <input type="checkbox" name="toggle" id="Gray" className="checked:bg-gray-500 outline-none focus:outline-none right-4 checked:right-0 duration-200 ease-in absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer" />
+      <label htmlFor="Gray" className="block h-6 overflow-hidden bg-gray-500 rounded-full cursor-pointer">
+      </label>
+    </div>
+    <span className="font-medium text-gray-400">
+      Gray
+    </span>
+  </div>
+</div>
+
+
+             </CodeBox>
+        </>
+    )
+}
+export default Toggle3;
