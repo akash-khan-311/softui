a7d491cf7fdc8f83c9e1840dc5b597b3326813f5
Akash Khan <akashcc48@gmail.com>
2024-03-07 16:03:35 +0600

    a speed dial and a navbar added

--- src/UIcomponents/Toggle/Toggle.jsx
+++ src/UIcomponents/Toggle/Toggle.jsx
@@ -1,9 +1,11 @@
 import Toggle1 from "../../components/App/Toggles/Toggle1";
+import Toggle2 from "../../components/App/Toggles/Toggle2";
 
 const Toggle = () => {
     return(
         <div>
              <Toggle1/>
+             <Toggle2/>
         </div>
     )
 }
