3999b4db5286b7ac059ebb6cdff92144b065f87a
Akash Khan <akashcc48@gmail.com>
2024-03-09 11:49:16 +0600

    feat: dropdown components put in the dropdown folder

--- src/UIcomponents/Dropdown/Dropdown.jsx
+++ src/UIcomponents/Dropdown/Dropdown.jsx
@@ -1,5 +1,5 @@
-import Dropdown2 from "../components/App/DropDown/DropDown2";
-import Dropdown1 from "../components/App/DropDown/Dropdown1";
+import Dropdown2 from "../../components/App/DropDown/DropDown2";
+import Dropdown1 from "../../components/App/DropDown/Dropdown1";
 
 const Dropdown = () => {
     return(
