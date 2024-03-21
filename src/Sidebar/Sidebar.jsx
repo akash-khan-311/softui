56fc1a19cc55cc4443a39e28a92c6379ed890ceb
Rasdanga GPS <akashcc48@gmail.com>
2024-03-21 15:27:02 +0600

    development branch updated , user can clone this branch without any confusion

--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -74,7 +74,7 @@ const Sidebar = () => {
         </button>
       </div>
       <div
-        className={` components-sidebar backdrop-blur-xl bg-white/10 fixed top-[70px] z-[20] ${
+        className={` components-sidebar backdrop-blur-xl bg-white/10 fixed top-[59px] z-[20] ${
           isActive && "-translate-x-full"
         }  xl:translate-x-0 xl:w-[240px]  select-none px-2 block transition-all overflow-y-scroll`}
       >
