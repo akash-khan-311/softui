d30a342f95e8398d6e73a7e04ff9b68e71bfa499
Akash Khan <akashcc48@gmail.com>
2024-03-23 11:21:35 +0600

    feat:range-slider: new components added

---
+++ src/UIcomponents/Range-Slider/Slider.jsx
@@ -0,0 +1,12 @@
+import Slider1 from "../../components/Range-Slider/Slider1";
+
+
+const Slider = () => {
+    return (
+        <>
+            <Slider1/>
+        </>
+    );
+};
+
+export default Slider;
