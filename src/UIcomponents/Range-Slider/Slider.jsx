328ae539833317b3954c8ebf2910f13e4eea891c
Akash Khan <akashcc48@gmail.com>
2024-03-23 11:48:55 +0600

    feat:slider: another slider updated

--- src/UIcomponents/Range-Slider/Slider.jsx
+++ src/UIcomponents/Range-Slider/Slider.jsx
@@ -1,10 +1,12 @@
 import Slider1 from "../../components/Range-Slider/Slider1";
+import Slider2 from "../../components/Range-Slider/Slider2";
 
 
 const Slider = () => {
     return (
         <>
             <Slider1/>
+            <Slider2/>
         </>
     );
 };
